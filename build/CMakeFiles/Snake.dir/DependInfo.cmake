af28bab675eb8145a284843c5f4c834d5952012c
nekrasovVM <NekraVM@yandex.ru>
2025-03-20 18:53:39 +0300

    Added cmake files

---
+++ build/CMakeFiles/Snake.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/media/nekra/Acer/Ubuntu/repos/Snake/Field.cpp" "CMakeFiles/Snake.dir/Field.cpp.o" "gcc" "CMakeFiles/Snake.dir/Field.cpp.o.d"
+  "/media/nekra/Acer/Ubuntu/repos/Snake/Game.cpp" "CMakeFiles/Snake.dir/Game.cpp.o" "gcc" "CMakeFiles/Snake.dir/Game.cpp.o.d"
+  "/media/nekra/Acer/Ubuntu/repos/Snake/Snake.cpp" "CMakeFiles/Snake.dir/Snake.cpp.o" "gcc" "CMakeFiles/Snake.dir/Snake.cpp.o.d"
+  "/media/nekra/Acer/Ubuntu/repos/Snake/main.cpp" "CMakeFiles/Snake.dir/main.cpp.o" "gcc" "CMakeFiles/Snake.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
